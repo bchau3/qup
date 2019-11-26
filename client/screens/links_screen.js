ebf20d7e9b9e856a6b25446a54d3555d146a04d5
David Zhao <coral8icepop@davids-mbp.dynamic.ucsd.edu>
2019-11-26 00:06:27 -0800

    undated UI for home and signIn screens

--- client/screens/links_screen.js
+++ client/screens/links_screen.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import {
-    AppRegistry,    // change background color
-    Image,
-    Platform,
-    ScrollView,
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-    View,
-    Button
+  AppRegistry,    // change background color
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Button
 } from 'react-native';
 
 import { createAppContainer, createSwitchNavigator, NavigationActions } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack'
 import HostQueueScreen from './host_queue_screen';
 import TestLogin from '../components/test_login';
+import { styles } from '../style/link_screen_style';
+import { Overlay } from 'react-native-elements';
 
 /*
  * LinksScreen:
@@ -24,44 +26,57 @@ import TestLogin from '../components/test_login';
  */
 class LinksScreen extends React.Component {
 
-    static navigationOptions = {
-        title: 'Link Account',
-        header: null,
-    }
+  static navigationOptions = {
+    title: 'Link Account',
+    header: null,
+  }
 
-    render() {
-        const { navigate } = this.props.navigation;
-        return (
-          <View style={styles.container}>
-            <ScrollView
-              style={styles.container}
-              contentContainerStyle={styles.contentContainer}
-            >
-              <View style={styles.getStartedContainer}>
-                <Text>Link your Spotify Premium Account!</Text>
-                <TouchableOpacity
-                  style={styles.buttonStyle}
-                  onPress={() => {
-                    navigate("SIGNIN");
-                  }}
-                  underlayColor="#fff"
-                >
-                  <Text style={styles.buttonText}>Link Account</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={styles.buttonStyle}
-                  onPress={() => {
-                    this.props.navigation.goBack(null);
-                  }}
-                  underlayColor="#fff"
-                >
-                  <Text style={styles.buttonText}>Cancel</Text>
-                </TouchableOpacity>
-              </View>
-            </ScrollView>
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View style={styles.scrollViewContainer}>
+
+        <View style={styles.LinkScreenContainer}>
+          <View style={{paddingBottom: 30}}>
+            <Image 
+              source={require("../assets/images/spotifyLogo.png")}
+              style={styles.imageStyle}
+            />
           </View>
-        );
-    }
+
+          <Text style={styles.textStyleLink}>Link to your</Text>
+          <View style={{paddingBottom: 15}}><Text style={styles.textStyleLink}>Spotify premium account!</Text>
+          </View>
+          
+          <TouchableOpacity
+            style={styles.buttonStyle}
+            onPress={() => {
+              generateCode(),
+              navigate("SIGNIN");
+            }}
+            underlayColor="#fff"
+          >
+            <Text style={styles.buttonText}>Link</Text>
+          </TouchableOpacity>
+
+
+          <TouchableOpacity
+            style={styles.buttonStyle2}
+            onPress={() => {
+              this.props.navigation.goBack(null);
+            }}
+          >
+            <Text style={styles.buttonText2}>Cancel</Text>
+          </TouchableOpacity>
+
+        </View>
+      </View>
+    );
+  }
+}
+
+function generateCode(){
+  alert('code: 12345');
 }
 
 /* SignInScreen: 
@@ -70,99 +85,59 @@ class LinksScreen extends React.Component {
  */
 class SignInScreen extends React.Component {
 
-    static navigationOptions = {
-        header: null,
-    }
-    render() {
-        return (
-          <View style={styles.container}>
-            <ScrollView
-              style={styles.container}
-              contentContainerStyle={styles.contentContainer}
+  static navigationOptions = {
+    header: null,
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <ScrollView
+          style={styles.container}
+          contentContainerStyle={styles.contentContainer}
+        >
+          <View style={styles.getStartedContainer}>
+            <Text>Provide login credentials!</Text>
+            <TouchableOpacity
+              style={styles.buttonStyle}
+              onPress={() => {
+                alert("Successfully linked account!"),
+                  this.props.navigation.navigate("HOST");
+              }}
+              underlayColor="#fff"
             >
-              <View style={styles.getStartedContainer}>
-                <Text>Provide login credentials!</Text>
-                <TouchableOpacity
-                  style={styles.buttonStyle}
-                  onPress={() => {
-                    alert("Successfully linked account!"),
-                      this.props.navigation.navigate("HOST");
-                  }}
-                  underlayColor="#fff"
-                >
-                  <Text style={styles.buttonText}>Success!</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={styles.buttonStyle}
-                  onPress={() => {
-                    alert("Failed to link your account..."),
-                      this.props.navigation.goBack(null);
-                  }}
-                  underlayColor="#fff"
-                >
-                  <Text style={styles.buttonText}>Fail!</Text>
-                </TouchableOpacity>
-              </View>
-            </ScrollView>
-            <TestLogin/>
+              <Text style={styles.buttonText}>Success!</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.buttonStyle}
+              onPress={() => {
+                alert("Failed to link your account..."),
+                  this.props.navigation.goBack(null);
+              }}
+              underlayColor="#fff"
+            >
+              <Text style={styles.buttonText}>Fail!</Text>
+            </TouchableOpacity>
           </View>
-        );
-    }
+        </ScrollView>
+        <TestLogin />
+      </View>
+    );
+  }
 }
 
 
 const HostFlow = createStackNavigator(
-    {
-        LINK: LinksScreen,
-        SIGNIN: SignInScreen,
-        HOST: HostQueueScreen,
-    },
-    {
-        headerMode: 'none'
-    }
+  {
+    LINK: LinksScreen,
+    SIGNIN: SignInScreen,
+    HOST: HostQueueScreen,
+  },
+  {
+    headerMode: 'none'
+  }
 );
 
 const AppContainer = createAppContainer(HostFlow);
 
 export default AppContainer;
 
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#89Cff0"
-  },
-  contentContainer: {
-    paddingTop: 30
-  },
-  getStartedContainer: {
-    fontSize: 20,
-    backgroundColor: "#89CFF0",
-    alignItems: "center",
-    marginHorizontal: 0,
-    marginVertical: 30
-  },
-  todoText: {
-    textAlign: "left",
-    fontSize: 15
-  },
-  buttonStyle: {
-    marginRight: 10,
-    marginLeft: 10,
-    marginTop: 10,
-    paddingTop: 10,
-    paddingBottom: 10,
-    backgroundColor: "#ffb6c1",
-    borderRadius: 50,
-    borderWidth: 1,
-    borderColor: "#000000",
-    width: 200
-  },
-  buttonText: {
-    color: "#000000",
-    textAlign: "center",
-    paddingLeft: 20,
-    paddingRight: 20,
-    fontSize: 15
-  }
-});
