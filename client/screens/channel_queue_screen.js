1a5b70208d4beeec9ed78f0c248879b28f0b63d1
David Zhao <coral8icepop@Davids-MacBook-Pro.local>
2019-11-25 21:25:03 -0800

    making search bar as an individual file

--- client/screens/channel_queue_screen.js
+++ client/screens/channel_queue_screen.js
@@ -5,10 +5,9 @@ import { Image, Platform, ScrollView, StyleSheet, Text, View, TouchableOpacity, 
 // for screen switch
 import { createBottomTabNavigator } from "react-navigation";
 import OptionScreen from "./option_screen";
-import { SearchBar } from "react-native-elements";
+import SearchBarScreen from "./search_bar_screen";
 
 // Get server info from config file
-import { server_url } from "../config.js";
 const queryString = require("query-string");
 
 import TabBarIcon from "../components/TabBarIcon"; // for bar icons
@@ -49,123 +48,6 @@ class ChannelQueueScreen extends React.Component {
       </View>
     );
   }
-}
-
-/* SearchBarScreen:
- *    This screen has a search bar allows user to search a song from Spotify
- */
-class SearchBarScreen extends React.Component {
-  state = {
-    search: "",
-    list: []
-  };
-
-  updateSearch = search => {
-    this.setState({ search });
-    this.songSearch(search);
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <SearchBar
-          inputStyle={{ backgroundColor: "#ffb6c1" }}
-          containerStyle={{
-            backgroundColor: "#DB7093",
-            borderWidth: 1,
-            borderRadius: 5
-          }}
-          inputContainerStyle={{ backgroundColor: "#ffb6c1" }}
-          placeholderTextColor={"#436EEE"}
-          placeholder={"Search for a song"}
-          onChangeText={this.updateSearch}
-          value={this.state.search}
-          showCancel={true}
-        />
-        <ScrollView
-          style={styles.container}
-          contentContainerStyle={styles.contentContainer}
-        >
-          <View style={styles.getStartedContainer} >
-            {this.state.list.map((song) => { 
-              return (
-                <TouchableOpacity
-                  style={styles.buttonStyle}
-                  onPress={() => {
-                    // TODO
-                  }}
-                  underlayColor="#fff"
-                >
-                  <View style={{paddingRight: 10 }}>
-                  <Image
-                    style={{ width: 50, height: 50}}
-                    source={{ uri: song.album_artwork }}
-                  />
-                  </View>
-
-                  <Text>
-                    <Text style={styles.songTitle}>
-                      {song.song_name}
-                      {"\n\n"}
-                    </Text>
-                    <Text>{song.artist_name}</Text>
-                  </Text>
-
-                  {/* <Text style={styles.buttonText}>{song.key}</Text> */}
-                  {/* <Text style={styles.buttonText}>{song.song_uri}</Text> */}
-                  {/* {<Text style={styles.buttonText}>{song.album_artwork}</Text>} */}
-                </TouchableOpacity>
-              );
-            })}
-          </View>
-        </ScrollView>
-      </View>
-    );
-  }
-
-  songSearch = async (query) => {
-    const channel_id = await this._getChannelId();
-    const encodedQuery = encodeURIComponent(query);
-    let response = await fetch(
-      `${server_url}/search?q=${encodedQuery}&channel_id=${channel_id}`
-    );
-    let responseText = await response.text();
-    let responseJSON = await JSON.parse(responseText);
-    console.log(responseJSON);
-    this.parseSongs(responseJSON);
-  };
-
-  parseSongs = responseJSON => {
-    this.setState({list: []});
-    for(var i = 0; i < 15; i++){
-      var track_id = responseJSON.body.tracks.items[i].album.artists[0].id;
-      var artist_name = responseJSON.body.tracks.items[i].album.artists[0].name;
-      var song_name = responseJSON.body.tracks.items[i].name;
-      var song_uri = responseJSON.body.tracks.items[i].album.artists[0].uri;
-      var album_artwork = responseJSON.body.tracks.items[i].album.images[2].url;
-
-      var json = JSON.parse(JSON.stringify({
-        key: track_id,
-        artist_name: artist_name,
-        song_name: song_name,
-        song_uri: song_uri,
-        album_artwork: album_artwork
-      }));
-      console.log(this.state.list);
-      this.setState({ list: this.state.list.concat(json) });
-    }
-  };
-
-  _getChannelId = async () => {
-  let channel_id = '';
-  try {
-    channel_id = await AsyncStorage.getItem('channel_id') || 'none';
-  } catch (error) {
-    // Error retrieving data
-    console.log(error.message);
-  }
-  return channel_id;
-}
 }
 
 // create bottom tabs to switch screens
